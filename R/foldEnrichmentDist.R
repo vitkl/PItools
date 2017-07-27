0684bd0cbb29b645f45eb911911514e81244ba51
Vitalii Kleshchevnikov <vitalii@petsalakigroup02-ml.windows.ebi.ac.uk>
2017-07-27 13:29:19 +0100

    fold enrichment pvals
    - set seed correctly for parallel processing (parSapply)
    - calculate Pvals
    - plot fold enrichment distribution for individual proteins

--- R/foldEnrichmentDist.R
+++ R/foldEnrichmentDist.R
@@ -1,6 +1,6 @@
-##' \code{foldEnrichmentDist} finds fold_enrichment distribution (in any domain or other feature) for each protein
+##' find fold_enrichment distribution (in any domain or other feature) for each protein in the network
 ##' @name foldEnrichmentDist
-##' @description \code{foldEnrichmentDist} finds fold_enrichment distribution (in any domain or other feature) for each protein in the first column of net. For more details: \code{\link{foldEnrichment}}
+##' @description \code{foldEnrichmentDist} finds fold_enrichment distribution (in any domain or other feature) for each protein in the first column of net. For more details: \code{\link[MItools]{foldEnrichment}}
 ##' @param net data.table that specifies the PPI network:
 ##' 1. first column specifies a protein ("IDs_interactor_viral")
 ##' 2. second column specifies it's interacting partner which annotations are of interest ("IDs_interactor_human")
@@ -9,12 +9,15 @@
 ##' 1. first column specifies a protein ("IDs_interactor_human")
 ##' 2. second column specifies features of that protein ("IDs_domain_human")
 ##' 3. third column specifies background frequency of those features ("domain_frequency)
-##' @return data.table containing fold enrichment for each domain - protein pair
+##' @param N number of times to run permutation of PPI network
+##' @param cores specify how many cores to use for parallel processing, default (NULL) is to detect all cores on the machine and use all minus one. When using LSF cluster you must specify the number of cores to use because \code{\link[BiocGenerics]{detectCores}} doen't know how much cores you have requested from LSF (with bsub -n) and detects all cores on the actual physical node.
+##' @param seed seed for RNG for reproducible sampling
+##' @return data.table containing fold enrichment for each domain (or other feature) - protein pair (2 columns: IDs_interactor_viral and sampled_fold_enrichment)
 ##' @author Vitalii Kleshchevnikov
 ##' @import data.table
 ##' @import BiocGenerics
 ##' @export foldEnrichmentDist
-foldEnrichmentDist = function(net, protein_annot, N = 1000, cores = NULL){
+foldEnrichmentDist = function(net, protein_annot, N = 1000, cores = NULL, seed = 1){
 
   # set up parallel processing
   # create cluster
@@ -24,7 +27,9 @@ foldEnrichmentDist = function(net, protein_annot, N = 1000, cores = NULL){
   clusterEvalQ(cl, {library(data.table); library(MItools)})
   # put objects in place that might be needed for the code
   clusterExport(cl, c("net", "protein_annot"), envir=environment())
-  # setorder(net, IDs_interactor_viral)
+  # set seed
+  clusterSetRNGStream(cl, iseed = seed)
+  # run permutations N times
   fold_enrichment_dist = parReplicate(cl, n = N,
                expr = {
                  net[, IDs_interactor_human := sample(IDs_interactor_human)]
