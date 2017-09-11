7937bb5cde99d262b32fa879c58aee40105af5db
vitkl <klv2706w@gmail.com>
2017-09-11 13:29:41 +0100

    Version: 0.1.22
    + plotEnrichment: our analysis vs ELM domains
    + permutResult2D plot features of top domain-protein pairs

---
+++ R/plotEnrichment.R
@@ -0,0 +1,70 @@
+##' Plot the enrichment of domains that are likely to be mediating interactions in domain annotated by ELM as mediating linear motif mediated interactions
+##' @name plotEnrichment
+##' @author Vitalii Kleshchevnikov
+##' @param ... objects (matrix) the output of \code{runningTestEnrichment}
+##' @param list list of object (alternative way to pass the output of \code{runningTestEnrichment})
+##' @param random_domains the result of comparing random domain-protein pairs (by \code{random_domains} function), can be null
+##' @param domains_known_mapped domains from ELM mapped to InterProID
+##' @param type what to plot: "pval"- Fisher test p-val (in out set vs in ELM), "odds_ratio" - Fisher test odds ratio, "count" of domains in our set that are in ELM
+##' @param plot_type r-base plot type default is "l"
+##' @return plot
+##' @import data.table
+##' @export plotEnrichment
+plotEnrichment = function(..., list = list(), random_domains = NULL, domains_known_mapped, type = "count", plot_type = "l", plot_name = ""){
+
+  res = list(...)
+  res = c(res, list)
+  typenum = match(type, c("pval", "odds_ratio", "count"))
+  ngroups = length(res)
+
+  if(type == "count") color = colorRampPalette(brewer.pal(7, "Dark2"))(ngroups + 1) else color = colorRampPalette(brewer.pal(7, "Dark2"))(ngroups)
+  if(is.na(typenum)) stop("'type' should be one of “count”, “odds_ratio”, “pval”")
+
+  leg_pos_y = max(sapply(res, function(x, typenum) max(as.numeric(x[typenum,])), typenum))
+  if(!is.null(random_domains)) leg_pos_y = max(leg_pos_y, random_domains[typenum][[1]])
+  if(type == "count") leg_pos_y = length(domains_known_mapped) - 1
+  leg_pos_x = max(sapply(res, function(x) max(as.numeric(colnames(x))))) * 0.20
+
+  if(type == "pval") {ylim = c(0, 1); ylab = "p-value"}
+  if(type == "count") {ylim = c(0,length(domains_known_mapped)+1); ylab = "known domain found"}
+  if(type == "odds_ratio") {ylim = c(0,leg_pos_y); ylab = "Fisher test odds ratio"}
+
+  plot(colnames(res[[1]]),rep(0,ncol(res[[1]])),
+       ylab = ylab, xlab = "top N viral protein - domain pairs selected",
+       type = plot_type, ylim = ylim, lwd = 0,
+       main = plot_name)
+  # plot random domains quantiles
+  if(!is.null(random_domains)){
+    random_legend = c("97.5% quantile", "75% quantile", "median", "25% quantile", "2.5% quantile")
+    random_cols = c("#DDDDDD", "#CCCCCC", "#AAAAAA", "#CCCCCC", "#DDDDDD")
+    random_line_width = c(2,4,8,4,2)
+    for (i in 1:5) {
+      lines(x = colnames(random_domains[typenum][[1]]), y = random_domains[typenum][[1]][random_legend[i],], col = random_cols[i], lwd = random_line_width[i], type = plot_type)
+    }
+  }
+
+  for (i in 1:ngroups) {
+    lines(x = colnames(res[[i]]), y = res[[i]][typenum,], col = color[i], type = plot_type, lwd = 3)
+  }
+
+  if(type == "count") abline(h = length(domains_known_mapped), col = color[ngroups + 1])
+
+  legend_names = c("statictic used in permutation test:")
+  for(i in 1:ngroups){
+    legend_names = c(legend_names, unique(res[[i]]["name",]))
+  }
+
+  if(type == "count") legend_names = c(legend_names, "domains known to interact with linear motifs")
+
+  line_width = rep(3, length(color) + 1)
+
+  if(!is.null(random_domains)){
+    legend_names = c(legend_names, paste0("N random protein-domain pairs, ", random_legend))
+    color = c(color, random_cols)
+    line_width = c(line_width, random_line_width)
+  }
+
+
+  legend(x = leg_pos_x, y = leg_pos_y, legend_names,
+         col = c("white", color), lty = 1, lwd = line_width, merge = TRUE)
+}
