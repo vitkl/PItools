fd179e219b67c1532a0506663c87b8b56aaf07b8
vitkl <klv2706w@gmail.com>
2017-10-19 12:26:29 +0100

    Version: 0.1.28 - CompariMotif3
    new:
    runCompariMotif3: a function that generated a system command and runs comparimotif_V3: http://rest.slimsuite.unsw.edu.au/docs&page=module:comparimotif_V3
    
    readQSLIMFinderMain, readQSLIMFinderOccurence, writePatternList: read (1,2) and preprocess (3) QSLIMFinder results for comparimotif_V3
    
    modified:
    runQSLIMFinder: clearer docs

---
+++ R/readQSLIMFinderMain.R
@@ -0,0 +1,51 @@
+##' Read and write QSLIMFinder results
+##' @rdname readQSLIMFinderMain
+##' @name readQSLIMFinderMain
+##' @author Vitalii Kleshchevnikov
+##' @param outputfile character vector of paths to QSLIMFinder main output files, one way to get these is \code{\link{writeInteractionSubsetFASTA_list}}()$outputfile
+##' @return readQSLIMFinderMain: data.table combined from all QSLIMFinder main output files. Contains summary statistic on each motif found in each dataset
+##' @import data.table
+##' @export readQSLIMFinderMain
+##' @seealso \code{\link{writeInteractionSubsetFASTA_list}}, \code{\link{runQSLIMFinder}}
+readQSLIMFinderMain = function(outputfile = "forSLIMFinder_file_list$outputfile"){
+  main_result = lapply(outputfile, function(file) {
+    #fread("./SLIMFinder/output/interactors_of.O60506_P03496./main_result", stringsAsFactors = F)
+    if(file.exists(file)) fread(file, stringsAsFactors = F)
+  })
+  Reduce(rbind, main_result)
+}
+
+##' Read and write QSLIMFinder results
+##' @rdname readQSLIMFinderMain
+##' @name readQSLIMFinderOccurence
+##' @author Vitalii Kleshchevnikov
+##' @param outputdir character vector of paths to QSLIMFinder output directories, one way to get these is \code{\link{writeInteractionSubsetFASTA_list}}()$outputdir
+##' @return readQSLIMFinderOccurence: data.table combined from all QSLIMFinder occurence output files. Contains positions of motifs in each protein.
+##' @import data.table
+##' @export readQSLIMFinderOccurence
+readQSLIMFinderOccurence = function(outputdir = "forSLIMFinder_file_list$outputdir"){
+  Occurence_list = lapply(outputdir, function(dir) {
+    if(dir.exists(dir)){
+      all_files = list.files(dir)
+      occurence_file = grep(".occ.csv", all_files, value = T)
+      if(length(occurence_file) == 1) fread(paste0(dir,occurence_file), stringsAsFactors = F)
+    }
+  })
+  Reduce(rbind, Occurence_list)
+}
+
+##' Read and write QSLIMFinder results
+##' @rdname readQSLIMFinderMain
+##' @name writePatternList
+##' @author Vitalii Kleshchevnikov
+##' @param QSLIMFinder_main_result data.table, the output of readQSLIMFinderMain()
+##' @param filename character, paths to file where to write motif list
+##' @return writePatternList: data.table containing Name and Pattern columns is saved as a tsv to \code{filename}, a format required for by comparimotif
+##' @import data.table
+##' @export writePatternList
+writePatternList = function(QSLIMFinder_main_result, filename = "./motifs.txt") {
+  motifs = unique(QSLIMFinder_main_result[,.(Dataset,Pattern)])
+  motifs[, Name := paste0(Dataset,Pattern)][,Dataset:=NULL]
+  fwrite(motifs, filename, sep = "\t")
+  return(motifs)
+}
---
+++ R/runCompariMotif3.R
@@ -0,0 +1,41 @@
+##' Run CompariMotif3
+##' @rdname runCompariMotif3
+##' @name runCompariMotif3
+##' @author Vitalii Kleshchevnikov
+##' @param input_file a path to a tsv file containing Name and Pattern columns
+##' @param slimpath path to /slimsuite/tools/
+##' @param dbpath path to directory where to save and keep ELM database (\link{http://elm.eu.org/}) or other database of linear motifs in a format required by comparimotif_V3: \link{http://rest.slimsuite.unsw.edu.au/docs&page=module:comparimotif_V3}
+##' @param dburl url where to download database
+##' @param parameters any commandline parameters for comparimotif_V3 as a single character string
+##' @param out_file path to a file where comparimotif_V3 should save the result
+##' @param LSF_project_path path to the project directory on the LSF cluster file system. If on LSF is to be used all other paths should be relative to this directory, alternatively, set this argument to empty character. Final path is \code{LSF_project_path} concatenated with \code{input_file}, \code{slimpath}, \code{out_file} or \code{dbpath}. When using this on a local machine set this argument to empty character.
+##' @param run logical, run comparimotif_V3 (T) or just output the command (F), can be useful for check correcteness
+##' @param with compare motifs in \code{input_file} to the database in \code{dbpath} ("db") or to \code{input_file} ("self")
+##' @return system command to be run or that was run
+##' @import data.table
+##' @export runCompariMotif3
+##' @seealso \code{\link{writeInteractionSubsetFASTA_list}}, \code{\link{runQSLIMFinder}}
+runCompariMotif3 = function(input_file = "./SLIMFinder/result/motifs.txt",
+                            slimpath = "../software/slimsuite/tools/",
+                            dbpath = "./data_files/",
+                            dburl = "http://elm.eu.org/elms/elms_index.tsv",
+                            parameters = "unmatched=T motific=T",
+                            out_file = "./SLIMFinder/result/comparimotif.tdt",
+                            LSF_project_path = "/hps/nobackup/research/petsalaki/users/vitalii/vitalii/viral_project/",
+                            run = F, with = c("self", "db")[2]){
+  if(with == "db"){
+    elm_filename = paste0(LSF_project_path, dbpath, Sys.Date(), "elms_index.tsv")
+    if(!file.exists(elm_filename)) download.file(dburl, elm_filename)
+  }
+  comparimotif_call = paste0("python ", LSF_project_path, slimpath, "comparimotif_V3.py")
+  input_res = paste0("motifs=", LSF_project_path, input_file)
+  if(with == "self"){
+    input_database = ""
+  } else if(with == "db"){
+    input_database = paste0("searchdb=", LSF_project_path, elm_filename)
+  } else stop("'with' is provided but is not 'self' or 'db'")
+  out_file = paste0("resfile=", LSF_project_path, out_file)
+  command = paste(comparimotif_call, input_res, input_database, parameters, out_file)
+  if(run) system(command, wait = F)
+  return(command)
+}
