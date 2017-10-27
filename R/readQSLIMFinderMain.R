ac37a5291ae150d798cebfb24b8a991441629059
vitkl <klv2706w@gmail.com>
2017-10-27 10:15:13 +0100

    readQSLIMFinderMain: handling empty files
    readQSLIMFinderMain: ignoring empty files, option: check which files are not empty

--- R/readQSLIMFinderMain.R
+++ R/readQSLIMFinderMain.R
@@ -3,14 +3,19 @@
 ##' @name readQSLIMFinderMain
 ##' @author Vitalii Kleshchevnikov
 ##' @param outputfile character vector of paths to QSLIMFinder main output files, one way to get these is \code{\link{writeInteractionSubsetFASTA_list}}()$outputfile
+##' @param check_exist if TRUE, check that each file exists / is not empty, if false - read all existing and non-empty files
 ##' @return readQSLIMFinderMain: data.table combined from all QSLIMFinder main output files. Contains summary statistic on each motif found in each dataset
 ##' @import data.table
 ##' @export readQSLIMFinderMain
 ##' @seealso \code{\link{writeInteractionSubsetFASTA_list}}, \code{\link{runQSLIMFinder}}
-readQSLIMFinderMain = function(outputfile = "forSLIMFinder_file_list$outputfile"){
+readQSLIMFinderMain = function(outputfile = "forSLIMFinder_file_list$outputfile", check_exist = F){
   main_result = lapply(outputfile, function(file) {
     #fread("./SLIMFinder/output/interactors_of.O60506_P03496./main_result", stringsAsFactors = F)
-    if(file.exists(file)) fread(file, stringsAsFactors = F)
+    if(check_exist){
+        if(file.exists(file)) {
+          if(file.size(file) > 0) exist = TRUE else exist = FALSE
+          } else exist = FALSE
+    } else if(file.exists(file)) if(file.size(file) > 0) fread(file, stringsAsFactors = F)
   })
   Reduce(rbind, main_result)
 }
@@ -23,12 +28,17 @@ readQSLIMFinderMain = function(outputfile = "forSLIMFinder_file_list$outputfile"
 ##' @return readQSLIMFinderOccurence: data.table combined from all QSLIMFinder occurence output files. Contains positions of motifs in each protein.
 ##' @import data.table
 ##' @export readQSLIMFinderOccurence
-readQSLIMFinderOccurence = function(outputdir = "forSLIMFinder_file_list$outputdir"){
+readQSLIMFinderOccurence = function(outputdir = "forSLIMFinder_file_list$outputdir", check_exist = F){
   Occurence_list = lapply(outputdir, function(dir) {
     if(dir.exists(dir)){
       all_files = list.files(dir)
       occurence_file = grep(".occ.csv", all_files, value = T)
-      if(length(occurence_file) == 1) fread(paste0(dir,occurence_file), stringsAsFactors = F)
+      file = paste0(dir,occurence_file)
+      if(check_exist){
+        if(file.exists(file)) {
+          if(file.size(file) > 0) exist = TRUE else exist = FALSE
+        } else exist = FALSE
+      } else if(length(occurence_file) == 1) if(file.size(file) > 0) fread(file, stringsAsFactors = F)
     }
   })
   Reduce(rbind, Occurence_list)
