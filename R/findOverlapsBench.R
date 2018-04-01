d6c41ff56d21aaf63fceff9d2fa2c4460ad34331
Vitalii Kleshchevnikov <vk7@sanger.ac.uk>
2018-04-01 17:40:58 +0100

    findOverlapsBench: suppress warnings
    findOverlapsBench: suppress warnings about different sequences being present in the occuring and benchmarking sets
    
    What to do if (line 33-42) all known GRanges overlap with something that we found
    
    comment code choosing only one p-value for the same range: no_overlap dataset - useless code: all predictions are 0 -

--- R/findOverlapsBench.R
+++ R/findOverlapsBench.R
@@ -13,12 +13,16 @@
 ##' @export findOverlapsBench
 ##' @seealso \code{\link{ELMdb2GRanges}}, \code{\link{GRangesINinteractionSubsetFASTA}}
 findOverlapsBench = function(occuring, benchmarking, predictor_col = "Sig", labels_col = "for_benchmarking", normalise = T, select_predictor_per_range = min, ...){
-  overlap_inds = findOverlaps(occuring, benchmarking, ...)
+  suppressWarnings({ #suppress warnings about different sequences being present in the occuring and benchmarking sets
+    overlap_inds = findOverlaps(occuring, benchmarking, ...)
+  })
   occuring_subset = occuring[queryHits(overlap_inds)]
   # benchmarkings_subset = benchmarkings[subjectHits(overlap_inds)]
   mcols(occuring_subset) = c(mcols(occuring_subset), mcols(benchmarking)[subjectHits(overlap_inds),])
 
-  no_overlap = subsetByOverlaps(benchmarking, occuring, invert = T, ...)
+  suppressWarnings({
+    no_overlap = subsetByOverlaps(benchmarking, occuring, invert = T, ...)
+  })
 
   # normalise, just in case predictor doesn't span the full range 0 ... 1
   if(normalise) mcols(occuring_subset)[,predictor_col] = mcols(occuring_subset)[,predictor_col]/max(mcols(occuring_subset)[,predictor_col])
@@ -26,9 +30,16 @@ findOverlapsBench = function(occuring, benchmarking, predictor_col = "Sig", labe
   res_overlap = data.frame(predictions = 1 - mcols(occuring_subset)[,predictor_col],
                            labels = mcols(occuring_subset)[,labels_col],
                            stringsAsFactors = F)
-  res_no_overlap = data.frame(predictions = 0,
-                              labels = mcols(no_overlap)[,labels_col],
-                              stringsAsFactors = F)
+  if(length(no_overlap) >= 1){
+    res_no_overlap = data.frame(predictions = 0,
+                                labels = mcols(no_overlap)[,labels_col],
+                                stringsAsFactors = F)
+  } else {
+    res_no_overlap = data.frame(predictions = numeric(),
+                                labels = numeric(),
+                                stringsAsFactors = F)
+  }
+
   # choose only one p-value for the same range: occuring_subset
   res_overlap_list = split(res_overlap, paste0(seqnames(occuring_subset), start(occuring_subset), end(occuring_subset)))
   res_overlap_list = lapply(res_overlap_list, function(res_over) {
@@ -36,13 +47,13 @@ findOverlapsBench = function(occuring, benchmarking, predictor_col = "Sig", labe
     unique(res_over)
   })
   res_overlap = Reduce(rbind, res_overlap_list)
-  # choose only one p-value for the same range: no_overlap
-  res_no_overlap_list = split(res_no_overlap, paste0(seqnames(no_overlap), start(no_overlap), end(no_overlap)))
-  res_no_overlap_list = lapply(res_no_overlap_list, function(res_over) {
-    res_over$predictions = select_predictor_per_range(res_over$predictions)
-    unique(res_over)
-  })
-  res_no_overlap = Reduce(rbind, res_no_overlap_list)
+  # choose only one p-value for the same range: no_overlap - useless code: predictions are 0
+  #res_no_overlap_list = split(res_no_overlap, paste0(seqnames(no_overlap), start(no_overlap), end(no_overlap)))
+  #res_no_overlap_list = lapply(res_no_overlap_list, function(res_over) {
+  #  res_over$predictions = select_predictor_per_range(res_over$predictions)
+  #  unique(res_over)
+  #})
+  #res_no_overlap = Reduce(rbind, res_no_overlap_list)
 
   res = rbind(res_overlap, res_no_overlap)
   list(overlapping_GRanges = occuring_subset, for_ROC = res)
